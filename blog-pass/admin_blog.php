a6c52c3ebf36145f47b7bb17f862b2d41ee62178
SuperAme <beatamez@hotmail.com>
2019-04-05 13:07:47 -0600

    cambio en admin

--- blog-pass/admin_blog.php
+++ blog-pass/admin_blog.php
@@ -77,7 +77,7 @@ if($user != "admin"){
                 </div> 
 
                 <div class="form-group">
-                    <textarea class="tinymce" name="texto" ></textarea>
+                    <textarea id="myTestarea" class="tinymce" name="texto" ></textarea>
                     
                 </div>
 
@@ -86,7 +86,49 @@ if($user != "admin"){
             </div>            
         </form>
         
-    </div>     
+    </div> 
+    <script>
+tinymce.init({
+    selector: '#myTextarea',
+    plugins: 'image code',
+    toolbar: 'undo redo | image code',
+    
+    // without images_upload_url set, Upload tab won't show up
+    images_upload_url: 'upload.php',
+    
+    // override default upload handler to simulate successful upload
+    images_upload_handler: function (blobInfo, success, failure) {
+        var xhr, formData;
+      
+        xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.open('POST', 'upload.php');
+      
+        xhr.onload = function() {
+            var json;
+        
+            if (xhr.status != 200) {
+                failure('HTTP Error: ' + xhr.status);
+                return;
+            }
+        
+            json = JSON.parse(xhr.responseText);
+        
+            if (!json || typeof json.location != 'string') {
+                failure('Invalid JSON: ' + xhr.responseText);
+                return;
+            }
+        
+            success(json.location);
+        };
+      
+        formData = new FormData();
+        formData.append('file', blobInfo.blob(), blobInfo.filename());
+      
+        xhr.send(formData);
+    },
+});
+</script>    
 </body>
 
 </html>
