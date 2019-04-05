3a6e09a8802a5089b6abd53a50dbc3ec3fdd10ff
armancma7777 <armancma7777@gmail.com>
2019-04-05 13:25:58 -0600

    cambios avanzados blog

---
+++ blog-pass/php/conexion7.php
@@ -0,0 +1,42 @@
+
+
+<?php
+	/*
+	Datos de la base de datos
+	*/
+	/*
+	$servidor="localhost";
+	$usuario="passcom_admin";
+	$password="2019_pass";
+	$basededatos="passcom_blog_pass";
+*/
+	$servidor="localhost";
+	$usuario="root";
+	$password="";
+	$basededatos="blog_pass";
+	
+
+	
+	/*
+	Creación de la conexión a la base de datos con m
+    ysql_connect()
+	*/
+	$conexion = mysqli_connect($servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+	
+	/* 
+	Selección de la base de datos a utilizar
+	*/
+    $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Hay un problema con el nombre de su base de datos" );
+
+    $consulta = " SELECT `texto` from `blog` WHERE id=2 ";
+
+    $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+    $fila7 = mysqli_fetch_array($resultado);
+
+	mysqli_close( $conexion );
+
+  
+    
+
+    ?>
