549190978e56630104daffff699a872c7980c0ff
SuperAme <beatamez@hotmail.com>
2019-04-08 17:02:35 -0500

    formulario admin

--- blog-pass/admin_blog.php
+++ blog-pass/admin_blog.php
@@ -16,15 +16,15 @@ if($user != "admin"){
     <link rel="stylesheet" href="css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <script src="./assets/js/blog.js"></script>
+    <script src="js/validaFormulario.js"></script>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="plugin/tinymce/tinymce.min.js"></script>
     <script type="text/javascript" src="plugin/tinymce/init-tinymce.js"></script>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <title>Administrador | PASS</title>
 </head>
@@ -54,7 +54,7 @@ if($user != "admin"){
 				-->
 		<form action="php/conexion.php" method ="POST" enctype="multipart/form-data" >
             <div class="form-group col-md-6">
-                    <select class="form-group" name="opciones">
+                    <select class="form-group" name="opciones" id="opciones">
                             <option value="valor_1">Últimas Noticias</option>
                             <option value="valor_2">Novedades Aspel 1</option>
                             <option value="valor_3">Novedades Aspel 2</option>
@@ -71,12 +71,12 @@ if($user != "admin"){
                 </div>
 
                 <div class="form-group">
-                    <label form = "correo">Resumen </label>
-                    <input type="text" name="resumen" placeholder= "Resumen de la nota " class="form-control input-sm" required>
+                    <label form = "correo" id="resumenLabel">Resumen </label>
+                    <input type="text" name="resumen" placeholder= "Resumen de la nota " class="form-control input-sm" id="resumenInput" required>
                 </div>
 
                 <div class="form-group">
-                        <label>Imagen:</label> 
+                        <label id="imagenLabel">Imagen:</label> 
                         <input id="imagen" name="imagen" type="file">
                 </div> 
 
--- blog-pass/js/validaFormulario.js
+++ blog-pass/js/validaFormulario.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-$("#opciones").val()
-console.log($("#opciones").val())
-
+    $("#opciones").on("change",function(){
+        var valor = $(this).val()
+        if (valor == "valor_5" || valor == "valor_6" || valor == "valor_7" || valor == "valor_8"){
+            $("#resumenLabel").hide()
+            $("#resumenInput").hide()
+            $("#imagenLabel").hide()
+            $("#imagen").hide()
+        }else{
+            $("#resumenLabel").show()
+            $("#resumenInput").show()
+            $("#imagenLabel").show()
+            $("#imagen").show()
+        }
+//        console.log($(this).val())
+    })
 })
