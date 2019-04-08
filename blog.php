0a937dfcbfaef318112a596ae6efac9c52985682
SuperAme <beatamez@hotmail.com>
2019-04-08 09:38:31 -0500

    test

--- blog.php
+++ blog.php
@@ -4,7 +4,7 @@
 <!--[if IE 7 ]>    <html lang="en" class="ie7">    <![endif]-->
 <!--[if IE 8 ]>    <html lang="en" class="ie8">    <![endif]-->
 <!--[if IE 9 ]>    <html lang="en" class="ie9">    <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!-->
+<!--[if (gt IE 9)|!(IE)]>test<!-->
 <html class="no-js" lang="es-MX">
     <head>
         <meta charset="utf-8">
